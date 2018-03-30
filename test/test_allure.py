93d3746f016214af88bf4e44d143969cf6064ff6
yomiqiu <37658474+yomiqiu@users.noreply.github.com>
2018-03-30 20:26:17 +0800

    allure

--- test/test_allure.py
+++ test/test_allure.py
@@ -1,11 +1,15 @@
+import allure
+
+
 class Test_allure:
 
 
     def setup(self):
-        assert 1
+        assert 0
 
     def teardown(self):
         assert 1
 
+    @allure.step('我是测试步骤001')
     def test_al(self):
         assert 1
